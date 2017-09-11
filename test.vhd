fbe7dcf78dcb15c90544f51d7a91afea373519bf
Brian Crafton <crafton.b@husky.neu.edu>
2017-09-10 20:15:38 -0400

    adding slave

--- test.vhd
+++ test.vhd
@@ -28,6 +28,28 @@ architecture arch_test of test is
     r_user =>   '0'
   );
 
+  constant slave_in : nasti_slave_in_type := (
+    aw_valid => '0',
+    aw_bits =>  META_NONE,
+    aw_id =>    (others =>'0'),
+    aw_user =>  '0',
+
+    w_valid =>  '0',
+    w_data =>   (others =>'0'),
+    w_last =>   '0',
+    w_strb =>   (others =>'0'),
+    w_user =>   '0',
+
+    b_ready =>  '0',
+
+    ar_valid => '0',
+    ar_bits =>  META_NONE,
+    ar_id =>    (others =>'0'),
+    ar_user =>  '0',
+
+    r_ready =>  '0'
+  );
+
   COMPONENT master
   PORT(
     clk : in std_logic;
@@ -37,24 +59,44 @@ architecture arch_test of test is
   );
   END COMPONENT;
 
+  COMPONENT slave
+  PORT(
+    clk : in std_logic;
+    si : in nasti_slave_in_type;
+    so : out nasti_slave_out_type;
+    cfg : out nasti_slave_config_type
+  );
+  END COMPONENT;
+
   -- inputs
   signal clk : std_logic := '0';
+
   signal mi : nasti_master_in_type := master_in; 
+  signal si : nasti_slave_in_type := slave_in;
 
   -- outputs
   signal mo : nasti_master_out_type;
-  signal cfg : nasti_master_config_type;
+  signal so : nasti_slave_out_type;
+  signal mcfg : nasti_master_config_type;
+  signal scfg : nasti_slave_config_type;
 
   -- clk period
   constant clk_period : time := 1 us;
 
 begin  
 
-  uut : master PORT MAP(
+  m_uut : master PORT MAP(
     clk => clk,
     mi => mi,
     mo => mo,
-    cfg => cfg
+    cfg => mcfg
+  );
+
+  s_uut : slave PORT MAP(
+    clk => clk,
+    si => si,
+    so => so,
+    cfg => scfg
   );
 
   clk_process : process begin
